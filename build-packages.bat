2140fc007da76d2f401724e86e09b62e40d5d375
Grahame Grieve <grahameg@gmail.com>
2019-02-10 20:02:31 +1100

    add packages for R4

--- build-packages.bat
+++ build-packages.bat
@@ -46,10 +46,36 @@ cd hl7.fhir.core\3.0.1
 "C:\Program Files\7-Zip\7z.exe" a -tgzip package.tgz package.tar
 cd ..\..
 
+cd hl7.fhir.core\4.0.0
+"C:\Program Files\7-Zip\7z.exe" a -ttar package.tar package other xml
+"C:\Program Files\7-Zip\7z.exe" a -tgzip package.tgz package.tar
+cd ..\..
+
+cd hl7.fhir.core.gen\1.0.2 
+"C:\Program Files\7-Zip\7z.exe" a -ttar package.tar package other xml
+"C:\Program Files\7-Zip\7z.exe" a -tgzip package.tgz package.tar
+cd ..\..
+
+cd hl7.fhir.core.gen\1.4.0
+"C:\Program Files\7-Zip\7z.exe" a -ttar package.tar package other xml
+"C:\Program Files\7-Zip\7z.exe" a -tgzip package.tgz package.tar
+cd ..\..
+
+cd hl7.fhir.core.gen\3.0.1
+"C:\Program Files\7-Zip\7z.exe" a -ttar package.tar package other xml
+"C:\Program Files\7-Zip\7z.exe" a -tgzip package.tgz package.tar
+cd ..\..
+
+cd hl7.fhir.core.gen\4.0.0
+"C:\Program Files\7-Zip\7z.exe" a -ttar package.tar package other xml
+"C:\Program Files\7-Zip\7z.exe" a -tgzip package.tgz package.tar
+cd ..\..
+
 del *.tar /s /q
 
 xcopy fhir.test.data\*.tgz upload\fhir.test.data /S
 xcopy fhir.tx.support\*.tgz upload\fhir.tx.support /S
 xcopy hl7.fhir.core\*.tgz upload\hl7.fhir.core /S
+xcopy hl7.fhir.core.gen\*.tgz upload\hl7.fhir.core.gen /S
 
 pause
