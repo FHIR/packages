f4a1583666ce13dd2ea7986a3482e48af54f71a1
Grahame Grieve <grahameg@gmail.com>
2019-07-05 20:51:49 +1000

    more re-org of packages

---
+++ build-random-package.bat
@@ -0,0 +1,9 @@
+del *.tgz /s /q
+del *.tar /s /q
+
+"C:\Program Files\7-Zip\7z.exe" a -ttar package.tar package
+"C:\Program Files\7-Zip\7z.exe" a -tgzip package.tgz package.tar
+
+del *.tar /s /q
+
+pause
